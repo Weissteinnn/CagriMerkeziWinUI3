13473bc4a63aaf3c8bb6acd1975b95d796da2424
Weissteinnn <batuhandeniz.aktas@outlook.com>
2025-04-22 21:12:42 +0300

    son gece

--- Proje_Aktarım/Proje_Aktarım/MainPage.xaml.cs
+++ Proje_Aktarım/Proje_Aktarım/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace Proje_Aktarim
 {
@@ -9,6 +10,11 @@ namespace Proje_Aktarim
         public MainPage()
         {
             this.InitializeComponent();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.DecodePixelWidth = 600; 
+            bitmap.UriSource = new Uri("ms-appx:///Assets/logo_optimized.png");
+            imgLogo.Source = bitmap;
+
         }
 
         private async void Yonetici_Click(object sender, RoutedEventArgs e)
